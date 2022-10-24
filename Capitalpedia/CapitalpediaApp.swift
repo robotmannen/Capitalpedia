3661f660533bc56938852abab705264ce73ffec2
Thomas Korsnes <89935350+Korsfareren@users.noreply.github.com>
2022-10-24 20:54:58 +0200

    Initial Commit

---
+++ Capitalpedia/CapitalpediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapitalpediaApp.swift
+//  Capitalpedia
+//
+//  Created by Thomas Korsnes on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CapitalpediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
